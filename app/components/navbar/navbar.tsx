9099e35734bcc108d1139c4a4059985f2dafc19a
agata <agata.grebenuk@.mail.ru>
2024-03-15 10:02:20 +0700

    Style the active linr navBar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import { GiAlienBug } from "react-icons/gi";
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 const Navbar = () => {
+    const currentPath = usePathname();
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' },
@@ -15,7 +20,12 @@ const Navbar = () => {
                 { links.map(link => 
                     <li key={link.href}>
                         <Link 
-                            className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                            // className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                            className={classnames({
+                                'text-zinc-900': link.href === currentPath,
+                                'text-zinc-500': link.href !== currentPath,
+                                'hover:text-zinc-800 transition-colors': true
+                            })}
                             href={link.href}
                         >
                             {link.label}
