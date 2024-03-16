0caf42131662ea16fb3cbb045c3e403e7a732307
agata <agata.grebenuk@.mail.ru>
2024-03-16 11:24:29 +0700

    issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,18 @@
+"use client"
 import React from 'react'
+
+import dynamic from 'next/dynamic';
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false });
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
     return (
         <div className="flex flex-col gap-y-5 max-w-xl">
             <input type="text" placeholder="Title" className="input input-bordered w-full" />
-            <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
-            <button className="btn btn-primary">button</button>
+            <SimpleMDE
+                options={{spellChecker: false}} 
+                className="textarea textarea-bordered" placeholder="Bio"></SimpleMDE>
+            <button className="btn btn-primary w-fit">button</button>
         </div>
     )
 }
